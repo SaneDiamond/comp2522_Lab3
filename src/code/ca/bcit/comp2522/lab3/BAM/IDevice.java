a7d2d4ab4f0678bc3ec6a3811569ff15fd8d7416
Ben Nguyen <benc.nguyen02@gmail.com>
2024-09-19 18:05:03 -0700

    owo uwu owo

--- src/code/ca/bcit/comp2522/lab3/BAM/IDevice.java
+++ src/code/ca/bcit/comp2522/lab3/BAM/IDevice.java
@@ -1,4 +1,21 @@
 package ca.bcit.comp2522.lab3.BAM;
 
-public class IDevice {
+// Importing the abstract class and interfaces
+public abstract class IDevice {
+
+    protected String purpose;
+
+    // Constructor
+    public IDevice(String purpose) {
+        this.purpose = purpose;
+    }
+
+    // Concrete method
+    public String getPurpose() {
+        return purpose;
+    }
+
+    // Abstract method to print details
+    public abstract void printDetails();
 }
+
