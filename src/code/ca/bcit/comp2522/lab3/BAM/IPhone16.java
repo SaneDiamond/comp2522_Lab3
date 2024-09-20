8fc4f0411c3da195d34fe42d531b658757e690d7
SaneDiamond <andredizon3@gmail.com>
2024-09-19 20:02:21 -0700

    added iphone 16 and iPhone validation

--- src/code/ca/bcit/comp2522/lab3/BAM/IPhone16.java
+++ src/code/ca/bcit/comp2522/lab3/BAM/IPhone16.java
@@ -1,7 +1,46 @@
 package ca.bcit.comp2522.lab3.BAM;
 
 public class IPhone16 extends IPhone {
+
+    private static boolean highResCam;
+    private static int memoryGig;
+
     public IPhone16(double phonePlanMins, String phoneCarrier) {
         super(phonePlanMins, phoneCarrier);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder iPhone16;
+        String iDeviceStr;
+        String iPhone16Str;
+
+        iPhone16 = new StringBuilder();
+        iDeviceStr = super.toString();
+        iPhone16.append(iDeviceStr).append("This iPhone having a res cam is : " + highResCam +
+                ". The amount of memory in the phone is" + memoryGig);
+        iPhone16Str = iPhone16.toString();
+        return iPhone16Str;
+    }
+
+    @Override
+    public boolean equals(final Object compareObj){
+        if (compareObj == null){
+            return false;
+        }
+
+        if (!(compareObj instanceof IPhone)){
+            return false;
+        }
+
+        if (compareObj == this){
+            return true;
+        }
+
+        final IPhone compareObjIPhone;
+        compareObjIPhone = (IPhone) compareObj;
+        final boolean equals;
+        equals = this.phonePlanMins==compareObjIPhone.phonePlanMins;
+        return equals;
     }
 }
