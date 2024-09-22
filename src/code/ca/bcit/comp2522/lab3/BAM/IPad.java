a51429f3b7d45bc9ca00e684bc9c1e1849bb31a1
Ben Nguyen <benc.nguyen02@gmail.com>
2024-09-22 13:27:48 -0700

    owo owo i love pushing onto branch

--- src/code/ca/bcit/comp2522/lab3/BAM/IPad.java
+++ src/code/ca/bcit/comp2522/lab3/BAM/IPad.java
@@ -1,98 +1,102 @@
 package ca.bcit.comp2522.lab3.BAM;
+
 import java.util.Objects;
 
 /**
- * IPAD
+ * IPad
  *
- * @author Ben,Andre,Marcus
+ * @author Ben, Andre, Marcus
  * Version 1.0
  */
 public class IPad extends IDevice {
     private final boolean hasCase;
     private final String systemVersion;
 
-    public IPad(final boolean hasCase ,final String systemVersion) {
+    /**
+     * Constructs an IPad with a case status and system version.
+     *
+     * @param hasCase whether the iPad has a case
+     * @param systemVersion the version of the iPad system
+     */
+    public IPad(final boolean hasCase, final String systemVersion) {
         super("learning");
-        //remember to validate
+        // Remember to validate
         systemVersionValidation(systemVersion);
-        hasCaseValidation(hasCase);
-
+        this.hasCase = hasCase; // Removed the unnecessary validation method
         this.systemVersion = systemVersion;
-        this.hasCase = hasCase;
     }
 
     /**
-     * Validating the system version
+     * Validates the system version.
      *
      * @param systemVersion the version of the system
      */
-    private static void  systemVersionValidation(final String systemVersion) {
+    private static void systemVersionValidation(final String systemVersion) {
         if (systemVersion == null || systemVersion.isEmpty()) {
-            throw new IllegalArgumentException("System version cannot be null");
+            throw new IllegalArgumentException("System version cannot be null or empty");
         }
     }
 
     /**
-     * Validating the case of the ipad
+     * Returns the purpose of the device.
      *
-     * @param hasCase check the case
+     * @return the purpose of this iDevice as a string
      */
-    private static void hasCaseValidation(final boolean hasCase) {
-        if (!hasCase) {
-            throw new IllegalArgumentException("hasCase cannot be null");
-        }
-    }
-    public String getPurpose(){
-
-        return "the purpose of this iDevice is “learning”";
+    public String getPurpose() {
+        return "The purpose of this iDevice is learning.";
     }
 
-    public String getDetails(){
+    /**
+     * Returns details of the iPad.
+     *
+     * @return details of the iPad as a string
+     */
+    public String getDetails() {
         return toString();
     }
-    public void printDetails(){
+
+    /**
+     * Prints the details of the iPad.
+     */
+    public void printDetails() {
         System.out.println(toString());
     }
+
     /**
-     * Retrieves the details of the ipad as a String.
+     * Retrieves the details of the iPad as a String.
      *
-     * @return an appended string
+     * @return a string representing whether it has a case and the system version
      */
     @Override
     public String toString() {
-        StringBuilder str =  new StringBuilder();
-
-        str.append("Do we have case? ")
+        StringBuilder str = new StringBuilder();
+        str.append("Do we have a case? ")
                 .append(this.hasCase)
-                .append(" system version: ")
+                .append(", system version: ")
                 .append(this.systemVersion);
         return str.toString();
     }
 
-
     /**
-     * Comapares the version of two ipads
+     * Compares the system version of two iPads.
      *
-     * @param version the version of the IPAD
-     * @return a comparison of two ipad version
+     * @param obj the object to compare to
+     * @return true if the system versions are the same, false otherwise
      */
     @Override
-    public boolean equals(final Object version) {
-        if (version == null){
-            return false;
-        }
-        if (!(version instanceof IPad)){
-            return false;
-        }
-        if (version == this){
+    public boolean equals(final Object obj) {
+        if (this == obj) {
             return true;
         }
-        // cast
-        final IPad version1 = (IPad) version;
-        return systemVersion.equalsIgnoreCase(version1.systemVersion);
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final IPad other = (IPad) obj;
+        return systemVersion.equalsIgnoreCase(other.systemVersion);
     }
+
     @Override
     public int hashCode() {
-        return Objects.hashCode("0");
+        return Objects.hash(systemVersion);
     }
 }
